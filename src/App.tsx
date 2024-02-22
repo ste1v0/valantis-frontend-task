bef4af73acec95aa1cbe2e6dca254a3c8b362cf7
ste1v0 <tyotarjous@yandex.ru>
2024-02-22 21:27:13 +0300

    feat: item component, make ids unique

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,27 @@
 import { useState, useEffect } from 'react'
 import md5  from 'md5'
 import './App.css'
+import Item from './components/Item'
 
 function App() {
 
 	const [ ids, setIds ] = useState([])
+	const [ items, setItems ] = useState([])
+	const [ loadingIds, setLoadingIds ] = useState(null)
+	const [ loadingItems, setLoadingItems ] = useState(null)
+	const [ itemIdClicked, setItemIdClicked ] = useState(false)
+
+	const currentDate = new Date()
+	const currentYear = currentDate.getFullYear()
+	const currentMonth = (currentDate.getMonth() + 1).toString().padStart(2, '0')
+	const currentDay = currentDate.getDate()
+
+	const password = process.env.API_PASSWORD
+
+	const key = md5(`${password}_${currentYear}${currentMonth}${currentDay}`)
 
 	useEffect(() => {
-		const currentDate = new Date()
-		const currentYear = currentDate.getFullYear()
-		const currentMonth = (currentDate.getMonth() + 1).toString().padStart(2, '0')
-		const currentDay = currentDate.getDate()
-
-		const password = process.env.API_PASSWORD
-
-		const key = md5(`${password}_${currentYear}${currentMonth}${currentDay}`)
-
+		setLoadingIds(true)
 		fetch('http://api.valantis.store:40000?', {
 			method: 'POST',
 			headers: {
@@ -23,7 +29,10 @@ function App() {
 				'X-Auth': key
 			},
 			body: JSON.stringify( {
-				action: 'get_ids'
+				action: 'get_ids',
+				params: {
+					limit: 50
+				}
 			})
 		})
 			.then(res => {
@@ -33,12 +42,59 @@ function App() {
 					throw new Error(`Whoops, the API has returned an error :( Code: ${res.status} Error Text: ${res.statusText}`)
 				}
 			})
-			.then(data => setIds(data.result))
+			.then(data => {
+				const uniqueValues = new Set(data.result)
+				setIds([...uniqueValues])
+				setLoadingIds(false)
+			})
 	}, [])
+
+	useEffect(() => {
+		if (ids.length > 0) {
+			setLoadingItems(true)
+			fetch('http://api.valantis.store:40000?', {
+				method: 'POST',
+				headers: {
+					'Content-Type': 'application/json',
+					'X-Auth': key
+				},
+				body: JSON.stringify( {
+					action: 'get_items',
+					params: {
+						ids: ids
+					}
+				})
+			})
+				.then(res => {
+					if (res.ok) {
+						return res.json()
+					} else {
+						throw new Error(`Whoops, the API has returned an error :( Code: ${res.status} Error Text: ${res.statusText}`)
+					}
+				})
+				.then(data => { 
+					setItems(data.result)
+					setLoadingItems(false)
+				})
+		}
+	}, [ids])
 
 	return (
 		<>
-			{ids.map(e => <p>{e}</p>)}
+			{(loadingIds || loadingItems) && 
+				<div className="app__loader-container">
+					<p className="app__loader">{loadingIds ? 'Loading IDs' : 'Loading products'}</p>
+				</div>
+			}
+			{!loadingIds && !loadingItems &&
+				<div className="app__items-container">
+					{items.map(e => {
+						return (
+							<Item key={e.id} brand={e.brand} price={e.price} product={e.product} id={e.id} />
+						)
+					})}
+				</div>
+			}
 		</>
 	)
 }
---
+++ src/components/Item.tsx
@@ -0,0 +1,22 @@
+import { useState } from 'react'
+
+export default function Item( { brand, price, product, id }) {
+
+    const [ itemIdClicked, setItemIdClicked ] = useState(false)
+
+    function handleIdClick() {
+		setItemIdClicked(prevValue => !prevValue)
+	}
+
+    return (
+        <>
+			<div className="app__item">
+			<div className="app__item-img" />
+			<h4 className="app__item-title">{brand ? `${brand}` : 'Brand unknown'}</h4>
+			<p className="app__item-product">{product ? `${product}` : 'Product name unknown'}</p>
+			<p className="app__item-price">{price ? `${price.toLocaleString("ru-RU", {style: 'currency', currency: 'RUB', minimumFractionDigits: 0})}` : 'Price upon request'}</p>
+			<p className="app__item-id pointer" onClick={handleIdClick}>{itemIdClicked ? `${id}` : 'Show ID'}</p>
+            </div>
+        </>
+    )
+}
